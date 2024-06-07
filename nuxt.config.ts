f95d9f4c27e2f936175bd028beba11ccb5d16b9c
m1rstan1978 <m1rstan@mail.ru>
2024-06-07 18:40:37 +0300

    fdas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,19 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ["~/assets/style/main.css"],
   plugins: ["~/plugins/animate.connect.js", "~/plugins/v-click-outside.js"],
+
   app: {
     pageTransition: {
       name: "page",
       mode: "out-in",
     },
   },
+  hooks: {
+    "render:route": (url, result, context) => {
+      result.html = result.html.replace(/rel="prefetch"/g, 'rel="preload"');
+    },
+  },
+
   ssr: true,
+  modules: ["@nuxt/image"],
 });
